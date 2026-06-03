9563ac44cce8a4faa738d5a00a0fda2d8b36f2f1
mahinder1998 <mahinderkumar710@gmail.com>
2026-06-03 19:11:50 +0530

    home page udpated

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
 <div class="flex items-center justify-center h-screen bg-blue-500">
     <h1 class="text-white text-6xl font-bold">
-        Tailwind Working 🚀
+        Tailwind Working hello 🚀
     </h1>
 </div>
 
