4f3d41e11eefa388e06c3b8d9bbd32d082099fff
mahinder1998 <mahinderkumar710@gmail.com>
2026-06-08 18:03:04 +0530

    content updated

--- header.php
+++ header.php
@@ -4,9 +4,21 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+   <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TQMFKQ2K');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body <?php body_class('font-sans text-black bg-white'); ?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQMFKQ2K"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <?php wp_body_open(); ?>
 
--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-<?php get_header(); ?>
+<?php
+get_header();
+?>
 
-<h1>Meziva Theme Working hello</h1>
+<main class="bg-white">
 
-<?php get_footer(); ?>
+    <?php if (is_cart() || is_checkout() || is_account_page()) : ?>
+
+        <div class="max-w-[1400px] mx-auto px-4 md:px-6 lg:px-8 py-8">
+            <?php
+            while (have_posts()) :
+                the_post();
+                the_content();
+            endwhile;
+            ?>
+        </div>
+
+    <?php else : ?>
+
+        <div class="max-w-[1400px] mx-auto px-4 md:px-6 lg:px-8">
+
+            <?php
+            while (have_posts()) :
+                the_post();
+                the_content();
+            endwhile;
+            ?>
+
+        </div>
+
+    <?php endif; ?>
+
+</main>
+
+<?php
+get_footer();
+?>
